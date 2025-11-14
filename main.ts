50b513e9dd9f3831324a0ddf33c19996403a2d38
John Santiago Arenales Fernandez <johnsaf@aulaestudio.es>
2025-11-14 18:39:39 +0100

    definicion robusta del inicio

--- main.ts
+++ main.ts
@@ -3,39 +3,62 @@ import dotenv from "dotenv";
 import { MongoClient,ObjectId } from "mongodb";
 dotenv.config();
 
-const puerto = 3000;
-const app = express();
-
-app.use(express());
-
-app.listen(puerto,() =>{
-      console.log("Escuchando en el puerto 3000 , http://localhost:"+puerto);
-});
 
 const NOMBRE_DB = process.env.db_nombre || "fichas_ae";
-const NOMBRE_COLECCION = "fichas"; 
 
 let coleccionPedidos: any;
 let coleccionMesas:any;
+let cliente:any;
 
-(
-      async() =>{
-            try {
-                  let cliente = new MongoClient(process.env.URI_MONGO || "mongodb://localhost:27017");
-                  await cliente.connect();
-                  coleccionMesas = cliente.db("sushi").collection("mesas");
-                  coleccionPedidos = cliente.db("suhi").collection("pedidos");
-            } catch (error) {
-                  console.error("Fallo conexion a mongo"+error)
-            }
 
-      }
-)();
+async function main() {
 
-async function obtenerColeccion_pedidos(coleccion:String) {
       try {
-            return(await coleccionPedidos.find({}).toArray());
+            cliente = new MongoClient(process.env.URI_MONGO || "mongodb://localhost:27017");
+            await cliente.connect();
+            coleccionMesas =await cliente.db("sushi").collection("mesas");
+            coleccionPedidos =await cliente.db("sushi").collection("pedidos");
+
+      } catch (error) {
+            console.error("Fallo conexion a mongo"+error)
+            process.exit(1); // Salir si la DB falla
+      }
+
+      const puerto = 3000;
+      const app = express();
+
+      app.use(express.json());
+
+      app.listen(puerto,() =>{
+            console.log("Escuchando en el puerto 3000, http://localhost:"+puerto);
+      });
+
+}
+main();
+
+
+
+async function obtenerColeccion_pedidos() {
+      try {
+            return ( await cliente.db("sushi").collection("pedidos").find().toArray() );
+      } catch (error) {
+            return {mensaje:"Error: " + error};
+      }
+}
+async function obtenerColeccion_mesas() {
+      try {
+            return( await cliente.db("sushi").collection("mesas").find().toArray() );
       } catch (error) {
             return{mensaje:"Error: " + error};
       }
 }
+async function obtenerColeccion_productos() {
+      try {
+            return( await cliente.db("sushi").collection("productos").find().toArray() );
+      } catch (error) {
+            return{mensaje:"Error: " + error};
+      }
+}
+
+
+
